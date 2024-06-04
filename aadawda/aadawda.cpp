4a58478defb0854f66eb6d3ecafb1ef4ff58fb50
FatSassin <hubert@talwik.com>
2024-06-04 13:26:22 +0200

    Dodaj pliki projektów.

---
+++ aadawda/aadawda.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+
+using namespace std;
+int main()
+{
+	//tworzenie tablicy z dostepnymi nominalami
+	int nominaly[] = { 500, 200, 100, 50, 20, 10, 5, 2, 1 };
+	int ile;
+	//tworzenie zmiennej kwota
+	int kwota;
+	int i;
+	cout << "Ile pieniedzy do wyplacenia: ";
+	cin >> kwota;
+	//tworzenie zmiennej ile
+
+	//dopuki kwota jest wieksza niz zero wykonuj petle
+	for (int i = 0; kwota > 0; i++)
+	{
+		if (kwota >= nominaly[i])
+		{
+			ile = kwota / nominaly[i];
+			kwota = kwota - (nominaly[i] * ile);
+			cout << nominaly[i] << " x " << ile << endl;
+		}
+	}
+}
